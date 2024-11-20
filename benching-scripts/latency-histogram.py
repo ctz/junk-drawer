92f1c1f8e01993ab2572368acece94c1b5a496a5
Joe Birr-Pixton <jpixton@gmail.com>
2024-11-20 16:15:15 +0000

    rev latency-histogram.py

--- benching-scripts/latency-histogram.py
+++ benching-scripts/latency-histogram.py
@@ -19,24 +19,56 @@ def max_bin(samples):
     return max(max(x) for x in samples)
 
 
-order = ("rustls 0.23.16", "OpenSSL 3.3.2")
+order = ("rustls 0.23.16", "OpenSSL 3.0.14", "OpenSSL 3.3.2", "BoringSSL")
+
+
+def stats(samples, title):
+    return (
+        "{}:\n- min: {:g}μs\n- mean: {:g}μs\n- std dev: {:g}μs\n- max: {:g}μs".format(
+            title, min(samples), np.mean(samples), np.std(samples), max(samples)
+        )
+    )
 
 
 for out_file, title, samples in [
     (
-        "tls13-resume.svg",
+        "latency-resume-tls13-server.svg",
         "TLS1.3 server resumption",
         [
             read("rustls/latency-resume-tls13-server.tsv"),
+            read("openssl-3.0.14/latency-resume-tls13-server.tsv"),
             read("openssl-3.3.2/latency-resume-tls13-server.tsv"),
+            read("boringssl/latency-resume-tls13-server.tsv"),
         ],
     ),
     (
-        "tls12-resume.svg",
+        "latency-resume-tls12-server.svg",
         "TLS1.2 server resumption",
         [
             read("rustls/latency-resume-tls12-server.tsv"),
+            read("openssl-3.0.14/latency-resume-tls12-server.tsv"),
             read("openssl-3.3.2/latency-resume-tls12-server.tsv"),
+            read("boringssl/latency-resume-tls12-server.tsv"),
+        ],
+    ),
+    (
+        "latency-fullhs-tls13-server.svg",
+        "TLS1.3 server full handshakes",
+        [
+            read("rustls/latency-fullhs-tls13-server.tsv"),
+            read("openssl-3.0.14/latency-fullhs-tls13-server.tsv"),
+            read("openssl-3.3.2/latency-fullhs-tls13-server.tsv"),
+            read("boringssl/latency-fullhs-tls13-server.tsv"),
+        ],
+    ),
+    (
+        "latency-fullhs-tls12-server.svg",
+        "TLS1.2 server full handshakes",
+        [
+            read("rustls/latency-fullhs-tls12-server.tsv"),
+            read("openssl-3.0.14/latency-fullhs-tls12-server.tsv"),
+            read("openssl-3.3.2/latency-fullhs-tls12-server.tsv"),
+            read("boringssl/latency-fullhs-tls12-server.tsv"),
         ],
     ),
 ]:
@@ -45,8 +77,8 @@ for out_file, title, samples in [
     bins = np.histogram_bin_edges(samples, bins=128, range=[0, max_bin(samples) * 0.75])
     bins[-1] = max_bin(samples)
     """
-    bins = np.histogram_bin_edges(samples, bins=128)
-    width = (bins[1] - bins[0]) * 0.5
+    bins = np.histogram_bin_edges(samples, bins=32)
+    width = (bins[1] - bins[0]) * 0.22
 
     f, (top, bot) = plt.subplots(2, sharex=True)
     top.set_title("Latency distribution")
@@ -56,12 +88,17 @@ for out_file, title, samples in [
     bot.hist(samples, bins=bins, log=True, width=width)
     bot.set_xlabel("Latency (microseconds)")
 
+    for i in range(len(order)):
+        f.text(
+            x=0.1 + (i * 0.2), y=0.05, s=stats(samples[i], order[i]), fontsize="small"
+        )
+
     for p in (bot, top):
         p.set_xlim(xmin=0)
         p.grid(axis="x")
-    f.subplots_adjust(hspace=0, left=0.1, right=0.95)
+    f.subplots_adjust(hspace=0, left=0.1, right=0.95, bottom=0.25)
     f.legend(order)
     f.suptitle(title)
     f.align_ylabels()
-    f.set_size_inches(10, 6)
+    f.set_size_inches(9, 6)
     plt.savefig(out_file, format="svg")
